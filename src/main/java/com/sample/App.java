8e5fa2f2531d77cae75d41b368d7dccf6d757fe8
SHYAM <SHYAM@DESKTOP-7QDF0JT>
2019-07-28 07:57:25 -0400

    for loop added

--- src/main/java/com/sample/App.java
+++ src/main/java/com/sample/App.java
@@ -10,5 +10,9 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Hi am from git 2");
+        
+        for(int i = 0;i<8;i++){
+        	System.out.println("i value"+i);
+        }
     }
 }
